39b52db13b77750341c0f72c3c63c50a0f1c43aa
l-huisman <64755533+l-huisman@users.noreply.github.com>
2022-03-09 12:51:38 +0100

    Create LecturerDAO.cs

--- Someren-master/SomerenModel/Teacher.cs
+++ Someren-master/SomerenModel/Teacher.cs
@@ -8,7 +8,17 @@ namespace SomerenModel
 {
     public class Teacher
     {
+        public int RoomID { get; set; }
         public string Name { get; set; }
+        public bool Supervisor { get; set; }
         public int Number { get; set; } // LecturerNumber, e.g. 47198
+
+        public Teacher(int teacher_ID, bool supervisor, int room_ID, string name)
+        {
+            Number = teacher_ID;
+            Supervisor = supervisor;
+            RoomID = room_ID;
+            Name = name;
+        }
     }
 }
