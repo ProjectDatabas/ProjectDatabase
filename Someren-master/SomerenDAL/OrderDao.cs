5efa78f7f93c2d43a44213c9c6cd256b464d7c16
CaptainJeppie <89974982+CaptainJeppie@users.noreply.github.com>
2022-03-18 17:18:09 +0100

    orderDao

---
+++ Someren-master/SomerenDAL/OrderDao.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using SomerenModel;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Collections.Generic;
+using System.Data;
+
+namespace SomerenDAL
+{
+    public class OrderDao : BaseDao
+    {
+        public List<Order> GetAllOrders()
+        {
+            string query = "SELECT * FROM receipt";
+            SqlParameter[] sqlParameters = new SqlParameter[0];
+            return ReadTables(ExecuteSelectQuery(query, sqlParameters));
+        }
+
+        private List<Order> ReadTables(DataTable dataTable)
+        {
+            List<Order> orders = new List<Order>();
+
+            foreach (DataRow dr in dataTable.Rows)
+            {
+                Order order = new Order()
+                {
+                    OrderId = (int)dr["OrderId"],
+                    StudentId = (int)(dr["StudentId"]),
+                };
+                orders.Add(order);
+            }
+            return orders;
+        }
+
+        public void SendOrder(Order order)
+        {
+            SqlParameter[] sqlParameters = { new SqlParameter("@StudentId", SqlDbType.Int) { Value = order.StudentId },
+                new SqlParameter("@DrinkId", SqlDbType.Int) { Value = order.DrinksIds[0] } };
+
+
+            try
+            {
+                ExecuteEditQuery("INSERT INTO [Order] (StudentId, DrinkId)" +
+                    "VALUES (@StudentId, @DrinkId);" +
+                    "SELECT SCOPE_IDENTITY();", sqlParameters);
+            }
+            catch (Exception e)
+            {
+                throw new Exception("Something went wrong with saving to the database. " + e.Message);
+            }
+
+
+        }
+    }
+}
