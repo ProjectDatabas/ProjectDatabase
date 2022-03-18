a4f735e3e6c900f7da057237edf630166f45cbe2
CaptainJeppie <89974982+CaptainJeppie@users.noreply.github.com>
2022-03-18 17:22:17 +0100

    drink

---
+++ Someren-master/SomerenModel/Drink.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SomerenModel
+{
+    public class Drink
+    {
+        public Drink(int drinkID, string drinksName, int drinksPrice, int stock)
+        {
+            DrinkId = drinkID;
+            DrinksName = drinksName;
+            DrinksPrice = drinksPrice;
+            Stock = stock;
+        }
+
+        public int DrinkId { get; }
+        public string DrinksName { get; }
+        public int DrinksPrice { get; set; }
+        public int Stock { get; set; }
+        public bool IsSufficientStock
+        {
+            get
+            {
+                return Stock > 10;
+            }
+        }
+    }
+}
