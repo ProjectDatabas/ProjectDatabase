897e7b03ae413ac1c5bddf5a922bee25ca88b685
CaptainJeppie <89974982+CaptainJeppie@users.noreply.github.com>
2022-03-18 17:20:47 +0100

    d

---
+++ Someren-master/SomerenLogic/OrderService.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using SomerenDAL;
+using SomerenModel;
+using System.Collections.Generic;
+
+namespace SomerenLogic
+{
+    public class OrderService
+    {
+        OrderDao orderdb;
+        public List<int> drinksIds = new List<int>();
+        public int student;
+
+        public OrderService()
+        {
+            orderdb = new OrderDao();
+        }
+
+        public List<Order> GetStudents()
+        {
+            List<Order> orders = orderdb.GetAllOrders();
+            return orders;
+        }
+
+        public void SendOrder(Order order)
+        {
+            orderdb.SendOrder(order);
+        }
+    }
+}
