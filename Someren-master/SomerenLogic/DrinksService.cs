16d9d1f278f5ea16770e2557bf1c0946f712c2de
CaptainJeppie <89974982+CaptainJeppie@users.noreply.github.com>
2022-03-18 17:19:42 +0100

    drinksservice

---
+++ Someren-master/SomerenLogic/DrinksService.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using SomerenDAL;
+using SomerenModel;
+using System.Collections.Generic;
+
+namespace SomerenLogic
+{
+    public class DrinksService
+    {
+        DrinksDao drinkdb;
+
+        public DrinksService()
+        {
+            drinkdb = new DrinksDao();
+        }
+
+        public List<Drinks> GetDrinks()
+        {
+            List<Drinks> drinks = drinkdb.GetAllDrinks();
+            return drinks;
+        }
+    }
+}
