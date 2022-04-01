b0663033b5d1c2084b0c5b05470c253acdaa39a6
l-huisman <64755533+l-huisman@users.noreply.github.com>
2022-04-01 13:59:04 +0200

    No build errors

--- Someren-master/SomerenLogic/HashWithSalt.cs
+++ Someren-master/SomerenLogic/HashWithSalt.cs
@@ -7,12 +7,12 @@ namespace SomerenLogic
 {
     // This code was acquired from here https://dotnetcodr.com/2016/10/17/how-to-hash-passwords-with-a-salt-in-net/
     // Author: "Andras Nemes"
-    public class Salt
+    public class HashWithSalt
     {
         public string Salt { get; }
         public string Digest { get; set; }
 
-        public Salt(string salt, string digest)
+        public HashWithSalt(string salt, string digest)
         {
             Salt = salt;
             Digest = digest;
--- Someren-master/SomerenLogic/LoginLogic.cs
+++ Someren-master/SomerenLogic/LoginLogic.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -12,6 +13,20 @@ namespace SomerenLogic
         {
             // Check if email and password are correct
             return email == "admin@inholland.nl" && password == "admin";
+        }
+
+        private static void TestPasswordHasher()
+        {
+            PasswordWithSaltHasher pwHasher = new PasswordWithSaltHasher();
+            HashWithSalt hashResultSha256 = pwHasher.HashWithSalt("ultra_safe_P455w0rD", 64, SHA256.Create());
+            HashWithSalt hashResultSha512 = pwHasher.HashWithSalt("ultra_safe_P455w0rD", 64, SHA512.Create());
+            HashWithSalt unHashedResultSha256 = pwHasher.HashWithSalt("ultra_safe_P455w0rD", 64, SHA256.Create());
+
+            Console.WriteLine(hashResultSha256.Salt);
+            Console.WriteLine(hashResultSha256.Digest);
+            Console.WriteLine();
+            Console.WriteLine(hashResultSha512.Salt);
+            Console.WriteLine(hashResultSha512.Digest);
         }
     }
 }
